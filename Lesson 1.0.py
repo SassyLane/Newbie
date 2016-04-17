34e3894d0c7a38434f4eb0aa13a0da9965dac78b
SassyLane <stacyrpr@yahoo.com>
2016-04-17 00:05:32 -0400

    Added files via upload

---
+++ Lesson 1.0.py
@@ -0,0 +1,42 @@
+Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 20:32:19) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name= "Stacy"
+>>> print "Hello" + name
+HelloStacy
+>>> print "Hello"  + name
+HelloStacy
+>>> print "Hello"  +  name
+HelloStacy
+>>> print "Hello" name
+SyntaxError: invalid syntax
+>>> print "Hello " + name
+Hello Stacy
+>>> len (Passion)
+
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    len (Passion)
+NameError: name 'Passion' is not defined
+>>> len ("Passion")
+7
+>>> language= ("Passion")
+>>> language * 7
+'PassionPassionPassionPassionPassionPassionPassion'
+>>> fortune * 7
+
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    fortune * 7
+NameError: name 'fortune' is not defined
+>>> fortune= ("Billions")
+>>> fortune * 7
+'BillionsBillionsBillionsBillionsBillionsBillionsBillions'
+>>> ("17) * 7
+ 
+SyntaxError: EOL while scanning string literal
+>>> ("17") * 7
+'17171717171717'
+>>> 17 * 7
+119
+>>> #Notes
+>>> 
